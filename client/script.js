4f2e387e938ec362c73c9916797f6a04a368e582
James Xalis <jamesxalis@icloud.com>
2023-01-12 16:10:27 -0700

    added uniqueID and typing

--- client/script.js
+++ client/script.js
@@ -17,3 +17,24 @@ function loader (element){
     }
   }, 300)
 }
+
+function typeText(element, text ){
+  let index =0;
+
+  let interval = setInterval(() => {
+    if (index < text.length){
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  },20)
+}
+
+function generateUniqueId(){
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
