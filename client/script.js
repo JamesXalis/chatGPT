0fc9f30fd91125270d6c0b850d263f6b7ceedb74
James Xalis <jamesxalis@icloud.com>
2023-01-12 16:36:48 -0700

    input and input on enter, fixed typos

--- client/script.js
+++ client/script.js
@@ -1,8 +1,8 @@
 import bot from './assets/bot.svg'
 import user from './assets/user.svg'
 
-const form = documet.queryselector('form');
-const chatContainer = documet.queryselecter('#chat_container');
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
 
@@ -38,3 +38,49 @@ function generateUniqueId(){
 
   return `id-${timestamp}-${hexadecimalString}`;
 }
+
+function chatStripe(isAi, value, uniqueId) {
+  return (
+      `
+      <div class="wrapper ${isAi && 'ai'}">
+          <div class="chat">
+              <div class="profile">
+                  <img 
+                    src=${isAi ? bot : user} 
+                    alt="${isAi ? 'bot' : 'user'}" 
+                  />
+              </div>
+              <div class="message" id=${uniqueId}>${value}</div>
+          </div>
+      </div>
+  `
+  )
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+
+  const data = new FormData(form)
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+  form.reset()
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId)
+
+  loader(messageDiv);
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) =>{
+  if (e.keyCode === 13){
+    handleSubmit(e);
+  }
+})
